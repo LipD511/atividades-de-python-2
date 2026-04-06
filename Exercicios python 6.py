7704ed3835a7597f485edf379f07629fc4ee6c01
Felipe Medeiros Cardoso <felipemeedeirospb@gmail.com>
2026-04-06 06:17:01 -0300

    feature: exercicio 6

---
+++ Exercicios python 6.py
@@ -0,0 +1,8 @@
+a = int(input("Digite o primeiro numero: "))
+b = int(input("Digite o segundo numero: "))
+if a > b:
+    print("O maior é", a)
+elif b > a:
+    print("O maior é", b)
+else:
+    print("Os números são iguais")
